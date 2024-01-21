35e7456bfd8c7f1e6d14f5570b8bdbd636cca85f
zion4d <zion4d@gmail.com>
2024-01-21 17:45:34 +0700

    redis ok

--- gembot.py
+++ gembot.py
@@ -3,45 +3,49 @@
 import os
 import logging
 import requests
-import time
+import redis
+import json
+import lxml
 
 from dotenv import load_dotenv
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from bs4 import BeautifulSoup
 
 
-# function to use
-MODE = 'proxy'  # gemini | proxy
-HTTPS_FILENAME = 'https.txt'
-
 # .env
 load_dotenv()
+PROXY = bool(os.getenv('PROXY'))
 TOKEN = os.getenv('GEMINI_CHAT_TOKEN')
 API_KEY = os.getenv("GOOGLE_API_KEY")
 MAX = int(os.getenv("MAX", 20))  # max proxies from list
 TIMEOUT = int(os.getenv("TIMEOUT", 20))  # seconds
 LIFETIME = int(os.getenv("LIFETIME", 600))  # seconds
+LOGLEVEL = int(os.getenv("LOGLEVEL", 30))
+
 
 # logging
 logging.basicConfig(
     filename="logfile",
     format="[%(asctime)s] func: %(funcName)s()\n%(message)s",
     datefmt="%Y-%m-%d %H:%M",
-    level=logging.WARNING,
+    level=LOGLEVEL,
     encoding="UTF-8",
 )
 
-# ask_gem_proxy()
-if MODE == 'proxy':
+
+PARSER = 'lxml'  # lxml | html.parser
+r_key = 'https_list'
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+
+if PROXY:
     url_https = 'https://www.sslproxies.org/'
     url_gemini = "https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent"
     headers = {
                 "Content-Type": "application/json",
                 "x-goog-api-key": API_KEY,
                 }
-
-# ask_gem()
-if MODE == 'gemini':
+else:
     import google.generativeai as genai
     
     genai.configure(api_key=API_KEY)
@@ -51,8 +55,8 @@ if MODE == 'gemini':
 def ask_gem(ask):
     try:
         response = model.generate_content(ask)
-        logging.warning('ASK: %s' % ask)
-        logging.warning('RESP: %s' % response.text)
+        logging.debug('ASK: %s' % ask)
+        logging.debug('RESP: %s' % response.text)
         return response.text
     
     except Exception as e:
@@ -60,57 +64,46 @@ def ask_gem(ask):
         return f"Exception: {e}"
     
 
-def get_https_file():
+def get_https() -> str:
     try:
         src = requests.get(url_https)
-        soup = BeautifulSoup(src.text, 'html.parser')
+        soup = BeautifulSoup(src.text, PARSER)
         https_str: str = soup.find(attrs={'class':'modal-body'}).find(attrs={'class':'form-control'}).text
 
         separator = ('\n\n')
         index: int = https_str.find(separator) + len(separator)
         https_str: str = https_str[index:]
-
-        with open(HTTPS_FILENAME, 'w') as f:
-            f.write(https_str)
-        # os.chmod(HTTPS_FILENAME, mode=0o777)
-        return https_str[:50]
+        https_list = https_str.split('\n')[:MAX]
+        set_https_list(https_list)
+        return https_str[:100] + '...'
     
     except Exception as e:
+        logging.error('Error: getting https from %s failed.' % url_https)
         return str(e)
     
 
-def check_https_file_ctime():
-    ctime = os.path.getctime(HTTPS_FILENAME)
-    lifetime = time.time() - ctime
-    return lifetime
+def get_https_list() -> list:
+    for _ in range(3):
+        https_list = r.get(r_key)
+        if https_list:
+            return json.loads(https_list)
+            return https_list.split('\n')
+        
+        get_https()
 
 
-def get_https_list():
-    lifetime = check_https_file_ctime()
-    if lifetime > LIFETIME:
-        get_https_file()
-    try:
-        with open(HTTPS_FILENAME, 'r') as f:
-            https_list = f.readlines()[:MAX]
-        return https_list
-    
-    except FileNotFoundError as e:
-        logging.error(str(e))
-
-
-def write_https_list(https_list: list):
-    try:
-        with open(HTTPS_FILENAME, 'w') as f:
-            f.writelines(https_list)
-
-    except IOError as e:
-        logging.error(str(e))
+def set_https_list(https_list: list):
+    https_str = json.dumps(https_list)
+    # https_str = '\n'.join(https_list)
+    r.set(r_key, https_str, ex=LIFETIME)
 
 
 def ask_gem_proxy(ask):
-    # https_list = open("https.txt", "r").read().strip().split("\n")[:20]
     data = {"contents":[{"parts":[{"text": ask}]}]}
     https_list = get_https_list()
+    if https_list is None:
+        return 'Не удалось загрузить халявные прокси.'
+    
     original = https_list.copy()
 
     while https_list:
@@ -122,7 +115,7 @@ def ask_gem_proxy(ask):
                 raise Exception('Exception: 400 User location is not supported for the API use.')
             
             if https_list != original:
-                write_https_list(https_list)
+                set_https_list(https_list)
 
             response = response.json()
             return response.get("candidates", [])[0].get("content", {}).get("parts", [])[0].get("text")
@@ -132,7 +125,7 @@ def ask_gem_proxy(ask):
             https_list.pop(0)
             continue
     
-    get_https_file()
+    get_https()
     return 'Халявные прокси не отвечают.\nПерегрузил список.\nПопробуй еще раз.'
 
 
@@ -142,52 +135,68 @@ async def start(update, context):
                                     text=msg)
 
 
-async def get_https(update, context):
-    res = get_https_file()
+async def run_get_https(update, context):
+    res = get_https()
     await context.bot.send_message(chat_id=update.effective_chat.id, 
                                     text=res)
     
 
-async def get_lifetime(update, context):
-    lifetime = check_https_file_ctime()
-    res = 'lifetime: %s' % lifetime
+async def get_redis(update, context):
+    lifetime = r.ttl(r_key)
+    https = r.get(r_key)
+    res = 'lifetime: %s\nhttps:\n%s' % (lifetime, https[:100] + '...')
     await context.bot.send_message(chat_id=update.effective_chat.id, 
                                     text=res)
     
 
 async def get_country(update, context):
     url = 'https://2ip.ru'
-    https = get_https_list()[0]
-    proxies = {'https': https.strip()}
-    response = requests.get(url, proxies=proxies)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    https = get_https_list()
+    if https is None:
+        res = 'Не удалось загрузить халявные прокси.'
+    else:
+        ip = https[0]
+        proxies = {'https': ip.strip()}
+        try:
+            response = requests.get(url, proxies=proxies)
+            soup = BeautifulSoup(response.text, PARSER)
 
-    ip = soup.find('div', class_='ip').text.strip()
-    country = soup.find('div', class_='value-country').text.strip().split('\n')[0]
+            ip = soup.find('div', class_='ip').text.strip()
+            country = soup.find('div', class_='value-country').text.strip().split('\n')[0]
+            
+            res = f"{ip} - {country}"
+
+        except Exception as e:
+            res = ip + '\n' + str(e)
+
+    await context.bot.send_message(chat_id=update.effective_chat.id, 
+                                    text=res)
     
-    res = f"{ip} - {country}"
+
+async def unknown(update, context):
+    res = 'unknown command'
     await context.bot.send_message(chat_id=update.effective_chat.id, 
                                     text=res)
 
 
 async def echo(update, context):
-    res = 'MODE is None'
-    if MODE == 'gemini':
-        res = ask_gem(update.message.text)
-    if MODE == 'proxy':
+    if PROXY:
         res = ask_gem_proxy(update.message.text)
+    else:
+        res = ask_gem(update.message.text)
 
     await context.bot.send_message(chat_id=update.effective_chat.id, 
                                     text=res)
     
 
 if __name__ == "__main__":
-    get_https_file()
+    get_https()
     app = Application.builder().token(TOKEN).build()
     app.add_handler(CommandHandler('start', start))
-    app.add_handler(CommandHandler('getproxy', get_https))
-    app.add_handler(CommandHandler('ctime', get_lifetime))
+    app.add_handler(CommandHandler('getproxy', run_get_https))
+    app.add_handler(CommandHandler('redis', get_redis))
     app.add_handler(CommandHandler('country', get_country))
+    app.add_handler(MessageHandler(filters.COMMAND, unknown))
     app.add_handler(MessageHandler(filters.TEXT, echo))
     app.run_polling()
 
